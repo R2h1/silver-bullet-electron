9a460fb7854eaff2dfdf3df716c7101e86ecfbff
R2h <1461694018@qq.com>
2024-08-11 23:21:29 +0800

    fix: 修复打包问题

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -4,7 +4,7 @@ import { electronAPI } from '@electron-toolkit/preload'
 // 渲染器的定制api
 const api = {}
 
-// 使用' contextBridge ' api只在启用上下文隔离的情况下将Electron api暴露给渲染器，否则只添加到DOM全局。
+// 使用 'contextBridge' api 只在启用上下文隔离的情况下将Electron api暴露给渲染器，否则只添加到DOM全局。
 if (process.contextIsolated) {
   try {
     contextBridge.exposeInMainWorld('electron', electronAPI)
